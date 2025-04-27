c87a25667e9303c53f66623356e600496d89e84a
Likhita Chinnari <lchinnar@asu.edu>
2025-04-27 14:16:08 -0700

    Fix icons visibility issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="stylesheet" href="/styles.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-p1CmR6Vf1dyPfYZ7R6pujISiFDUFxIr05o2N8S1Ry6j8Tz6kRXKuX3sI5Gucs5cjox96D65V6pZeRAwIJ5zs/Q==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </head>
       <body>{children}</body>
     </html>
